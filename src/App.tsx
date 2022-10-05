0db284935dba1a8966855952b11f7f0cdfb7500d
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-10-05 16:04:57 +0100

    setup: initial app setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+      rerum iure tenetur voluptate exercitationem explicabo, veritatis
+      dignissimos quis corporis modi maxime omnis dolorem saepe optio culpa
+      consectetur id praesentium temporibus.
+    </div>
+  )
+}
+
+export default App
