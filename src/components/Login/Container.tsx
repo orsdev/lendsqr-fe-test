5278978510586dabd74a4931f757197f9eca1704
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-10-09 09:03:20 +0100

    feat: login container styling modified

--- src/components/Login/Container.tsx
+++ src/components/Login/Container.tsx
@@ -1,5 +1,5 @@
-import React, { FC, ReactNode } from 'react'
-import Logo from '../../assets/images/logo.svg'
+import { Logo } from 'components/contants/assets'
+import { FC, ReactNode } from 'react'
 
 interface IProps {
   children: ReactNode
