51f897e71e0f9d2a13ede8ffe84184eb66d317a5
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-10-10 07:08:41 +0100

    feat: update userMonthlyIncomeRange utils

--- src/utills/userMonthlyIncomeRange.tsx
+++ src/utills/userMonthlyIncomeRange.tsx
@@ -1,9 +1,17 @@
 export const userMonthlyIncomeRange = (income: string[]) => {
-  if (income === null || income === undefined)
+  if (income === null || income === undefined) {
     return {
       min: 0,
       max: 0
     }
+  }
+
+  if (income.length === 0) {
+    return {
+      min: 0,
+      max: 0
+    }
+  }
 
   const convertIncomeToInterger = income.map((item) => Number(item))
 
