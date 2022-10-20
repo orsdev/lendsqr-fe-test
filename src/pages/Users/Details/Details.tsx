b687074b93117effd1ba8d3471e3236135e0f0f6
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-10-20 13:33:07 +0100

    feat: add error handling

--- src/pages/Users/Details/Details.tsx
+++ src/pages/Users/Details/Details.tsx
@@ -1,3 +1,4 @@
+import { Button, Result } from 'antd'
 import Spinner from 'common/Spinner'
 import AppLayout from 'components/Layout/Layout'
 import Container from 'components/Users/Details/Container'
@@ -7,7 +8,11 @@ import { Outlet, useParams } from 'react-router-dom'
 const Details = () => {
   const { id } = useParams()
 
-  const { data: user, isLoading }: any = useGetRequest({
+  const {
+    data: user,
+    isLoading,
+    error
+  }: any = useGetRequest({
     url: `/users/${id as string}`,
     key: ['get-single-user', id],
     options: {
@@ -18,6 +23,18 @@ const Details = () => {
   return (
     <AppLayout>
       {isLoading === true && <Spinner tip="Retrieving user details..." />}
+      {Boolean(error) && (
+        <Result
+          status="500"
+          title="500"
+          subTitle="Sorry, something went wrong."
+          extra={
+            <Button type="primary" onClick={() => window.location.reload()}>
+              Reload Browser
+            </Button>
+          }
+        />
+      )}
       {Boolean(user) && (
         <>
           <Container user={user} />
